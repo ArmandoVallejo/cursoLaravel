e4555856ecc551404150cbbbe94f71b99aeb713f
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2025-10-20 15:50:31 -0600

    feat (routes): :sparkles: Add a route to CRUD view of the example

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function(){
+    return view('test');
+});
+
+Route::get('/crud', function(){
+
+    $data = ['name' => 'Armando', 'age' => 33];
+
+    return view('crud/index',$data);
+})->name('crud');
