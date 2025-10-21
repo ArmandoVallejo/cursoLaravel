fd6aa7d8915ecbe48dd545ea81d26c10ccbe561c
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2025-10-21 14:26:36 -0600

    feat :sparkles: Create a register of a post in the DB

--- app/Http/Controllers/Dashboard/PostController.php
+++ app/Http/Controllers/Dashboard/PostController.php
@@ -13,7 +13,18 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        Post::create(
+            [
+                'title' => 'Test title',
+                'slug' => 'test-slug',
+                'content' => 'Test content',
+                'category_id' => 1,
+                'description' => 'Test description',
+                'posted' => 'no',
+                'image' => 'test-image.jpg'
+            ]
+        );
+        return 'Index';
     }
 
     /**
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'slug',
+        'content',
+        'category_id',
+        'description',
+        'posted',
+        'image'
+    ];
 }
