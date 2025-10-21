3af673cd557fea10814ee5ec8cb2b82273298e7a
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2025-10-21 12:08:41 -0600

    feat (layout): :sparkles: Create the master layout

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Contact 1</h1>
+@extends('master')
+
+@section('content')
+<h1>Contact 1</h1>
     <p>{{ $name }}</p>
 
     @if ($name != 'Juan')
@@ -21,6 +15,4 @@
             <li>Item {{ $item }}</li>
         @endforeach
     </ul>
-    
-</body>
-</html>
+@endsection
