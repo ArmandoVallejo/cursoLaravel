391051a1b8271799ef85a1fa7ba85b6b9d05b759
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2025-10-21 12:43:04 -0600

    feat (compact): :sparkles: Add the compact function to pass variables to the view

--- app/Http/Controllers/PrimerControlador.php
+++ app/Http/Controllers/PrimerControlador.php
@@ -7,7 +7,9 @@ use Illuminate\Http\Request;
 class PrimerControlador extends Controller
 {
     function index() {
-        return view('contact', ['name' => 'Juan']);
+        $posts = ['post1', 'post2'];
+        // return view('contact',['posts'=>$posts]);
+        return view('contact', compact('posts'));
     }
     //Puede recibir argumentos por defecto , es decir si no se envía nada toma el valor por defecto $post=40
     function otro($post, $otro) {
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,17 +2,5 @@
 
 @section('content')
 <h1>Contact 1</h1>
-    <p>{{ $name }}</p>
-
-    @if ($name != 'Juan')
-        <p>Tu nombre no es Juan</p>
-    @else
-        <p>Hola Juan!</p>
-    @endif
-
-    <ul>
-        @foreach ([1,2,3,4,5] as $item)
-            <li>Item {{ $item }}</li>
-        @endforeach
-    </ul>
+    <p>{{ $posts[0] }}</p>
 @endsection
