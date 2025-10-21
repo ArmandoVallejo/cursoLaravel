e5181b6c58da7a31c08bcfc1f499222138480193
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2025-10-21 12:18:43 -0600

    feat (controller): :sparkles: Create the first controller

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,10 @@
 <?php
 
+use App\Http\Controllers\PrimerControlador;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contact', function () {
-
-    //es lo mismo redirect->route que to_route
-    //return redirect()->route('contact2');
-    //return to_route('contact2');
-    // // //
-    //return redirect('/contact2');
-
-    return view('contact', ['name' => 'Juan']);
-})->name('contact');
-
-Route::get('/contact2', function () {
-    return view('contact2');
-})->name('contact2');
-
+Route::get('test', [PrimerControlador::class, 'index']);
