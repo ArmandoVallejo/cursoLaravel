b38bf95b6f58f239f46ab559aae96611348acd3e
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2025-10-22 15:15:33 -0600

    feat (show): :sparkles: Show the information of an individual post

--- app/Http/Controllers/Dashboard/PostController.php
+++ app/Http/Controllers/Dashboard/PostController.php
@@ -47,7 +47,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        
+        return view('dashboard.post.show', compact('post'));
     }
 
     /**
