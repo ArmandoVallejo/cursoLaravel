95130fa3d2f31330b423f408b2938f926a95d417
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2025-10-22 13:20:18 -0600

    feat (crud): :sparkles: Add the CRUD options to the posts

--- resources/views/dashboard/post/index.blade.php
+++ resources/views/dashboard/post/index.blade.php
@@ -1,20 +1,31 @@
 @extends('dashboard.master')
 
 @section('content')
+
+<a href="{{ route('post.create') }}">Crear un post</a>
 <table>
     <thead>
         <tr>
+            <th>Id</th>
             <th>Title</th>
             <th>Posted</th>
             <th>Category</th>
+            <th>Options</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($posts as $post)
             <tr>
+                <td>{{ $post->id }}</td>
                 <td>{{ $post->title }}</td>
                 <td>{{ $post->posted }}</td>
                 <td>{{ $post->category->title }}</td>
+                <td>
+                    <ul>
+                        <li><a href="{{ route('post.edit', $post->id) }}">Edit</a></li>
+                        <li><a href="{{ route('post.show', $post->id) }}">Show</a></li>
+                    </ul>
+                </td>
             </tr>
         @endforeach
     </tbody>
