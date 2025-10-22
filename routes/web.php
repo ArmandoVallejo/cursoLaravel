fce06e3bd576a0db8c1a545bb1b0ebb823daa245
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2025-10-22 16:01:16 -0600

    feat (group): :sparkles: Add the routes to a group

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('post', PostController::class);
-
-Route::resource('category', CategoryController::class);
+Route::group(['prefix' => 'dashboard'], function () {
+    Route::resource('post', PostController::class);
+    Route::resource('category', CategoryController::class);
+});
