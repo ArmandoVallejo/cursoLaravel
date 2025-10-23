edcd782e55c9dff604949c5a568427209ed12ddc
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2025-10-23 11:45:01 -0600

    feat (getAll): :sparkles: Create the methods and routes to get all the posts and categories

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -15,6 +15,10 @@ class CategoryController extends Controller
         return response()->json(Category::paginate(10));
     }
 
+    public function all(){
+        return response()->json(Category::get());
+    }
+
     public function store(StoreRequest $request)
     {
         $category = Category::create($request->validated());
