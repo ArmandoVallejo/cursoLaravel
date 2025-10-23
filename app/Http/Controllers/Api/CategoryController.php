3b6dff3d7707971c60491f930302023abd377175
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2025-10-23 11:21:27 -0600

    feat (apiCategories): :sparkles: Create the full api rest for categories

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Category\PutRequest;
+use App\Http\Requests\Category\StoreRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -10,26 +12,29 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        //
+        return response()->json(Category::paginate(10));
     }
 
-    public function store(Request $request)
+    public function store(StoreRequest $request)
     {
-        //
+        $category = Category::create($request->validated());
+        return response()->json($category, 201);
     }
 
     public function show(Category $category)
     {
-        //
+        return response()->json($category);
     }
 
-    public function update(Request $request, Category $category)
+    public function update(PutRequest $request, Category $category)
     {
-        //
+        $category->update($request->validated());
+        return response()->json($category);
     }
 
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return response()->json("ok", 204);
     }
 }
--- app/Http/Requests/Category/StoreRequest.php
+++ app/Http/Requests/Category/StoreRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\Category;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
 
 class StoreRequest extends FormRequest
 {
@@ -12,6 +14,14 @@ class StoreRequest extends FormRequest
     public function authorize(): bool
     {
         return true;
+    }
+
+    public function failedValidation(\Illuminate\Contracts\Validation\Validator $validator)
+    {
+        if($this->expectsJson()){
+            $response = new Response($validator->errors(), 422);
+            throw new ValidationException($validator, $response);
+        }
     }
 
     /**
